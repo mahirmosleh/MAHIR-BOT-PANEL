50230cc1e520253400dfea4c9fb355a8f72432c8
mahirmosleh <mahirmosleh11@gmail.com>
2026-09-25 01:03:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4498,4 +4498,4 @@ if __name__ == '__main__':
     ║       DB Download Password: MAHIRDB                      ║
     ╚══════════════════════════════════════════════════════════╝
     """)
-    app.run(host='0.0.0.0', port=8090, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
